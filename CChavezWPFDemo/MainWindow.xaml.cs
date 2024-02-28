aa81dd94b7c914f2eb4d82d2e1e54b9a52007a7c
HitandFeyd <cchavez572@cnm.edu>
2024-02-27 19:17:40 -0700

    after class commit

--- CChavezWPFDemo/MainWindow.xaml.cs
+++ CChavezWPFDemo/MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPFDemo;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace CChavezWPFDemo
 {
@@ -19,6 +21,65 @@ namespace CChavezWPFDemo
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            //////  4.6.	Add the following code:
+            //////Instantiate a class object
+            ////Student st = new Student();
+
+            //////Set the values from our controls into the class
+            ////st.FirstName = txbFirstName.Text;
+            ////st.LastName = txbLastName.Text;
+            ////st.StudentNumber = txbStudentNum.Text;
+            ////st.Major = txbMajor.Text;
+
+            //////Set the results from the class into a control on the form
+            ////txbResults.Text = st.ToString();
+            ///
+            //Instantiate a class object
+            Student st = new Student();
+
+            //  5.9.	Add the following code to the btnSubmit_Click method:
+            //Set the values from our controls into the class
+            st.FirstName = txbFirstName.Text;
+            st.LastName = txbLastName.Text;
+            st.StudentNumber = txbStudentNum.Text;
+            st.Major = txbMajor.Text;
+
+            //////  5.15.	Fix the code by changing it to the following:
+            ////List<int> scores = new List<int>();
+            ////foreach (int score in lbScores.Items)
+            ////{
+            ////    scores.Add(score);
+            ////}
+            ////st.Scores = scores;
+
+            //  6.8.	In the btnSubmit_Click event change the code that sets the contents of the list box to:
+            List<Assignment> scores = new List<Assignment>();
+            foreach (Assignment score in lbScores.Items)
+            {
+                scores.Add(score);
+            }
+            st.Scores = scores;
+
+            //Set the results from the class into a control on the form
+            txbResults.Text = st.ToString();
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            //////  5.3.	Double click on the button to add an event handler for it:
+            ////lbScores.Items.Add(int.Parse(txbScore.Text));
+            ///
+            //  6.7.Change the add button click event to:
+            Assignment assign = new Assignment();
+            assign.Title = txbTitle.Text;
+            assign.Score = int.Parse(txbScore.Text);
+            lbScores.Items.Add(assign);
+
         }
     }
 }
--- CChavezWPFDemo/Student.cs
+++ CChavezWPFDemo/Student.cs
@@ -3,6 +3,7 @@
 // Date: 20161003
 // Purpose: Holds information for one student
 
+using CChavezWPFDemo;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,30 +24,38 @@ namespace WPFDemo
         public string Major { get; set; }
         public double Average { get; private set; }
 
-        private List<int> scores;
+        //   instanitation method from live demo
+        private List<Assignment> scores = new List<Assignment>();
 
-        public List<int> Scores
+        ////public List<int> Scores 
+        ////{
+        ////    get { return scores; }
+        ////    set 
+        ////    {
+        ////        //Validate list of scores pased in as value
+        ////        bool isValid = true;
+        ////        foreach(int score in value)
+        ////        {
+        ////            isValid = isValid && score >= 0 && score <=100;
+        ////        }
+        ////        if (isValid) //it's valid, store it in the class
+        ////        {
+        ////            scores = value; 
+        ////            CalcAverage(); 
+        ////        }
+        ////        else//it's not valid, throw an exception
+        ////        {
+        ////            throw new ArgumentOutOfRangeException("Scores must be between 0 and 100");
+        ////        }
+        ////    }
+        ////}
+        ///6.3.	Because we are now doing the validation for scores in the Assignment class we can simplify the Scores property in the student class:
+        public List<Assignment> Scores
         {
             get { return scores; }
-            set 
-            {
-                //Validate list of scores pased in as value
-                bool isValid = true;
-                foreach(int score in value)
-                {
-                    isValid = isValid && score >= 0 && score <=100;
-                }
-                if (isValid) //it's valid, store it in the class
-                {
-                    scores = value; 
-                    CalcAverage(); 
-                }
-                else//it's not valid, throw an exception
-                {
-                    throw new ArgumentOutOfRangeException("Scores must be between 0 and 100");
-                }
-            }
+            set { scores = value; CalcAverage(); }
         }
+
         #endregion
 
         #region Constructors
@@ -81,6 +90,8 @@ namespace WPFDemo
             FirstName = firstName;
             LastName = lastName;
             Major = maj;
+            //  5.12.	Go into the class and add the following to our main constructor (the one all the other contstructors link to):
+            //  scores = new List<int>();
         }
 
         /// <summary>
@@ -95,7 +106,7 @@ namespace WPFDemo
         /// <param name="score3">Third score</param>
         /// <param name="maj"></param>
         public Student(string sID, string firstName, string lastName,
-            string maj, List<int> scores)
+            string maj, List<Assignment> scores)
             : this(sID, firstName, lastName, maj)
         {
             this.scores = scores;
@@ -107,12 +118,23 @@ namespace WPFDemo
         /// <summary>
         /// Calculates score average
         /// </summary>
+        ////public void CalcAverage()
+        ////{
+        ////    double sum = 0;
+        ////    foreach (int score in scores)
+        ////    {
+        ////        sum += score;
+        ////    }
+        ////    Average = sum / scores.Count;
+
+        ////}
+        /// 6.5.	We also need to change the CalculateAverage method to:
         public void CalcAverage()
         {
             double sum = 0;
-            foreach(int score in scores)
+            foreach (Assignment assignment in scores)
             {
-                sum += score;
+                sum += assignment.Score;
             }
             Average = sum / scores.Count;
         }
